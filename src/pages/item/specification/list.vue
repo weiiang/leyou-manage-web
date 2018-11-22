3851e571e1e4e5faec933fa7783c27ba2592e790
wq <wei.qiang@successinfo.com.cn>
2018-11-22 22:12:53 +0800

    规格参数分组和规格参数列表切换

--- src/pages/item/specification/list.vue
+++ src/pages/item/specification/list.vue
@@ -10,9 +10,8 @@
           />
         </v-card>
       </v-flex>
-      <!--规格参数组
-      -->
-      <v-flex  xs9>
+      <!--规格参数组-->
+      <v-flex  xs9 v-if="showGroup">
         <v-card  color="secondary">
           <v-toolbar flat color="white">
             <v-toolbar-title>规格参数组列表</v-toolbar-title>
@@ -22,13 +21,9 @@
               vertical
             ></v-divider>
             <v-spacer></v-spacer>
-            <v-dialog v-model="dialog" max-width="500px">
+            <v-dialog v-model="dialog" max-width="800px">
               <v-btn slot="activator" color="primary" dark class="mb-2">新建</v-btn>
               <v-card>
-               <!-- <v-card-title>
-                  <span style="background-color: #409EFF" class="headline">{{isEditSpecGroup ? "编辑规格参数组" : "新建规格参数在组"}}</span>
-                </v-card-title>-->
-                <!--对话框的标题-->
                 <v-toolbar dense dark color="primary">
                   <v-toolbar-title>{{isEditSpecGroup ? "编辑规格参数组" : "新建规格参数在组"}}</v-toolbar-title>
                   <v-spacer/>
@@ -63,16 +58,89 @@
             :items="specGroupList"
             class="elevation-1"
           >
-            <template slot="items" slot-scope="props">
-              <td class="text-xs-center">{{ props.item.id }}</td>
-              <td class="text-xs-center">{{ props.item.name }}</td>
-              <td class="justify-center layout px-0">
-                <v-btn icon @click="editBrand(props.item)"><i class="el-icon-edit"/></v-btn>
-                <v-btn icon @click="deleteBrand(props.item)"><i class="el-icon-delete"/></v-btn>
-              </td>
+            <template slot="items" slot-scope="props" >
+              <!--<tr @click="showSpecParam(props.item.id)" > </tr>-->
+                <td class="text-xs-center" >{{ props.item.id}}</td>
+                <td class="text-xs-center, nameStyle" style="" @click="showSpecParam(props.item.id)">{{ props.item.name }}</td>
+                <td class="justify-center layout px-0">
+                  <v-btn icon @click="editSpec(props.item)"><i class="el-icon-edit"/></v-btn>
+                  <v-btn icon @click="deleteSpec(props.item)"><i class="el-icon-delete"/></v-btn>
+                </td>
+
+
             </template>
             <template slot="no-data">
-              <v-btn color="primary" @click="initialize">Reset</v-btn>
+              <v-btn color="primary" click="initialize">Reset</v-btn>
+            </template>
+          </v-data-table>
+        </v-card>
+      </v-flex>
+
+      <v-flex  xs9 v-if="!showGroup">
+        <v-card  color="secondary">
+          <v-toolbar flat color="white">
+            <v-toolbar-title @click="backToGroup"><v-btn slot="activator" color="primary" dark class="mb-2">返回规格组</v-btn></v-toolbar-title>
+            <v-divider
+              class="mx-2"
+              inset
+              vertical
+            ></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" max-width="500px">
+              <v-btn slot="activator" color="primary" dark class="mb-2">新建</v-btn>
+              <v-card>
+                <v-toolbar dense dark color="primary">
+                  <v-toolbar-title>{{isEditSpecGroup ? "编辑规格参数组" : "新建规格参数在组"}}</v-toolbar-title>
+                  <v-spacer/>
+                  <!--关闭窗口的按钮-->
+                  <v-btn icon @click="closeWindow">
+                    <v-icon>close</v-icon>
+                  </v-btn>
+                </v-toolbar>
+                <v-card-text>
+                  <v-container grid-list-md>
+                    <v-layout wrap>
+                      <v-flex xs12 sm12>
+                        <v-text-field v-model="editedItem.name" label="分组名称" :rules="nameRules" required></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-layout class="my-4" row>
+                    <v-spacer/>
+                    <v-btn @click="submitSpecGroup" color="primary">提交</v-btn>
+                    <v-btn @click="clearSpecGroup">重置</v-btn>
+                  </v-layout>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+          <v-data-table
+            :headers="sparamHeaders"
+            :items="specParamList"
+            class="elevation-1"
+          >
+            <template slot="items" slot-scope="props" >
+              <tr>
+                <td class="text-xs-center">{{ props.item.id }}</td>
+                <td class="text-xs-center">{{ props.item.name }}</td>
+                <td class="text-xs-center">{{ props.item.numeric ? "是" :"否"}}</td>
+                <td class="text-xs-center">{{ props.item.unit }}</td>
+                <td class="text-xs-center">{{ props.item.generic ? "是" :"否" }}</td>
+                <td class="text-xs-center">{{ props.item.searching ? "是" :"否" }}</td>
+                <td class="text-xs-center">{{ props.item.segments }}</td>
+                <td class="justify-center layout px-0">
+                  <v-btn icon @click="editBrand(props.item)"><i class="el-icon-edit"/></v-btn>
+                  <v-btn icon @click="deleteBrand(props.item)"><i class="el-icon-delete"/></v-btn>
+                </td>
+              </tr>
+
+            </template>
+            <template slot="no-data">
+              <v-btn color="primary" click="initialize">Reset</v-btn>
             </template>
           </v-data-table>
         </v-card>
@@ -99,16 +167,47 @@
                   sortable: true,
                   value: 'name'
                 },
-                { text: '组名',align: 'center', value: 'name' },
+                { text: '组名',align: 'center', value: 'name',
+
+                },
+                { text: '操作', align: 'center', value: 'name', sortable: false }
+              ],
+              sparamHeaders: [
+                {
+                  text: 'ID',
+                  align: 'center',
+                  sortable: true,
+                  value: 'name'
+                },
+                { text: '参数名称',align: 'center', value: 'name' },
+                { text: '数字类型',align: 'center', value: 'numeric' },
+                { text: '单位',align: 'center', value: 'unit' },
+                { text: '通用属性',align: 'center', value: 'generic' },
+                { text: '是否搜索',align: 'center', value: 'searching' },
+                { text: '搜索条件',align: 'center', value: 'segments' },
                 { text: '操作', align: 'center', value: 'name', sortable: false }
               ],
               editedItem: {},
               defaultItem: {},
 
               specGroupList: [],//规格参数组列表
+              specParamList:[], //规格参数列表
+              showGroup: true, //显示规格组
             }
         },
         methods: {
+          //返回显示规格组
+          backToGroup(){
+            this.showGroup = true;
+          },
+
+          //点击规格组显示规格参数
+          showSpecParam(specGroupId){
+              this.$http.get("/item/spec-param/"+specGroupId).then(res =>{
+                  this.specParamList = res.data;
+                  this.showGroup = false;
+              })
+          },
           closeWindow(){this.dialog = !this.dialog},
           initialize(){},
           submitSpecGroup(){
@@ -121,12 +220,19 @@
             params.name = this.editedItem.name;
 
           },
+          editSpec(item){
+            alert(1)
+          },
+          deleteSpec(item){
+            alert(2)
+          },
           clearSpecGroup(){},
           handleClick(node){
             if(!node.isParent){
               this.currentNode = node;
               this.$http.get("/item/spec-group/"+node.id).then(res =>{
                 this.specGroupList = res.data;
+                this.showGroup = true;
               });
             }else{
               this.currentNode = {};
@@ -150,3 +256,13 @@
     }
 
 </script>
+
+<style>
+
+  .nameStyle{
+    /*background-color: rebeccapurple;*/
+    text-align: center;
+    color: blue;
+    cursor: pointer;
+  }
+</style>
